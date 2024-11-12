5df28379e1d95ccc089c3c6ffeadbf1e1b629b8a
andres-linares <andres.linares.971122@gmail.com>
2024-11-12 18:48:23 -0500

    feat: add initial favorites

--- src/composables/useFavorites.ts
+++ src/composables/useFavorites.ts
@@ -1,11 +1,25 @@
-import { ref } from "vue";
+import { useQueries, useQuery } from "@tanstack/vue-query";
+import { computed, ref } from "vue";
+import { api } from "../api";
 
 const FAVORITES_KEY = "favorites";
 
 export const useFavorites = () => {
-  const favoriteIds = ref(
-    JSON.parse(localStorage.getItem(FAVORITES_KEY) || "[]"),
-  );
+  const page = ref(1);
+
+  const favoriteIds = ref(JSON.parse(localStorage.getItem(FAVORITES_KEY) || "[]"));
+
+  const query = useQueries({
+    queries: favoriteIds.value.map((id: number) => {
+      return {
+        queryKey: ["joke", id],
+        queryFn: () => api.getJokeById(id),
+      };
+    }),
+    combine: (results) => {
+      return { data: results.map((result) => result.data) };
+    },
+  });
 
   const addFavorite = (id: number) => {
     favoriteIds.value.push(id);
@@ -13,11 +27,9 @@ export const useFavorites = () => {
   };
 
   const removeFavorite = (id: number) => {
-    favoriteIds.value = favoriteIds.value.filter(
-      (jokeId: number) => jokeId !== id,
-    );
+    favoriteIds.value = favoriteIds.value.filter((jokeId: number) => jokeId !== id);
     localStorage.setItem(FAVORITES_KEY, JSON.stringify(favoriteIds.value));
   };
 
-  return { addFavorite, removeFavorite };
+  return { query, addFavorite, removeFavorite };
 };
--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -1,1 +1,9 @@
-<template>holi</template>
+<script setup lang="ts">
+import { useFavorites } from "../composables/useFavorites";
+
+const { query } = useFavorites();
+</script>
+
+<template>
+  {{ query.data }}
+</template>
