e85154ac8314ea6265252763d1eadaf569b5c7e6
andres-linares <andres.linares.971122@gmail.com>
2024-11-12 17:39:10 -0500

    feat: prepare initial files

---
+++ src/api/index.ts
@@ -0,0 +1,7 @@
+export const api = {
+  getRandomJoke: () => {},
+  getRandomJokes: () => {},
+  getRandomJokesByType: (type: string) => {},
+  getJokeById: (id: number) => {},
+  getJokeTypes: () => {},
+};
