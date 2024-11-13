02c7ea1975c5e562526f2fa423546a744ca9dcbd
andres-linares <andres.linares.971122@gmail.com>
2024-11-12 19:12:38 -0500

    lint: add more eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,35 @@
 import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
 import js from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
 import tseslint from "typescript-eslint";
 
-export default [
+export default tseslint.config(
   js.configs.all,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
   ...pluginVue.configs["flat/recommended"],
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: true,
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".vue"],
+      },
+    },
+  },
+  {
+    files: ["**/*.js"],
+    ...tseslint.configs.disableTypeChecked,
+    rules: {
+      ...tseslint.configs.disableTypeChecked.rules,
+      "sort-keys": "off",
+      "no-magic-numbers": "off",
+    },
+  },
   eslintConfigPrettier,
-];
+);
